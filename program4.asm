af4f742b0cbc02ac059f3290086c645832ae8869
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-10-22 18:46:58 +0500

    Lab 2 Started

---
+++ program4.asm
@@ -0,0 +1,25 @@
+;program to add two numbers
+
+.model small
+.stack 100h
+.data
+
+
+.code                  
+
+
+Main Proc
+
+mov bl,1
+mov cl,2
+add bl,2
+mov al,bl
+
+
+Main EndP
+End Main
+
+
+
+
+
