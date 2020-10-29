bcab6d8af89467b06a51de59397fa13e9df068d9
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-10-29 18:10:36 +0500

    LECT-3

---
+++ program9.asm
@@ -0,0 +1,26 @@
+;Program to negate 5h using neg instruction 
+
+.model small
+.stack 100h
+.data
+
+
+.code                  
+
+Main Proc
+mov ax,5h
+neg ax
+mov dx,ax
+mov ah,2
+int 21h
+mov ah,4ch
+int 21h
+          
+Main EndP
+End Main
+
+ret
+
+
+
+
