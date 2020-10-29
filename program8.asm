9774a07973f19fcf15830e2fd47f063c068bfb43
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-10-29 17:55:04 +0500

    LECT-3

---
+++ program8.asm
@@ -0,0 +1,30 @@
+;Program to decrement an operand by inc instruction 
+
+.model small
+.stack 100h
+.data
+
+var1 db 63
+
+.code                  
+
+Main Proc
+
+mov ax,@data
+mov ds,ax
+mov al,var1
+dec al
+mov dl,al
+mov ah,2
+int 21h
+mov ah,4ch
+int 21h          
+          
+Main EndP
+End Main
+
+ret
+
+
+
+
