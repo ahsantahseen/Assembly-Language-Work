001b1544226bc5d71089441bed2cde9925a0544d
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-11-01 17:29:19 +0500

    Strings in  Assmebly

---
+++ program11.asm
@@ -0,0 +1,30 @@
+
+; Program to print a string on screen
+
+.model small
+.stack 100h
+.data
+s1 db "Ahsan$"
+
+.code     
+   
+Main Proc
+          
+                       
+   mov ax,@data
+   mov ds,ax       
+   
+   
+   lea dx,s1
+   mov ah,9
+   int 21h
+   
+   mov ah,4ch
+   int 21h
+Main EndP
+End Main
+
+
+
+
+
