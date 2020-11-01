9f3574e362286db63d2091ea34e59c67e7c3d067
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-11-01 17:55:04 +0500

    Strings in  Assmebly

--- program12.asm
+++ program12.asm
@@ -4,9 +4,9 @@
 .stack 100h
 .data
 
-s1 db "Ahsan$"
-s2 db "1912310$"
-s3 db "COAL$"
+s1 db  "Ahsan$"
+s2 db 10,13, "1912310$"   ;10,13 are for new line break
+s3 db 10,13, "COAL$"
 
 .code                  
 
@@ -17,30 +17,30 @@ mov ds,ax
 lea dx,s1
 mov ah,9
 int 21h
+Comment!
 mov dl,10
 mov ah,2
 int 21h
 mov dl,13
 mov ah,2
-int 21h   
+int 21h!  
+ 
 lea dx,s2
 mov ah,9
 int 21h
-mov dl,10
+
+Comment!
+mov dl,10 
 mov ah,2
 int 21h
 mov dl,13
 mov ah,2
-int 21h   
+int 21h!  
+
 lea dx,s3
 mov ah,9
 int 21h
-mov dl,10
-mov ah,2
-int 21h
-mov dl,13
-mov ah,2
-int 21h   
+
 
 mov ah,4ch
 int 21h
