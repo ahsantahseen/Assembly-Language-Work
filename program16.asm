c174d65c96933ceaa956b59b0f840d4a9a8792b9
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-11-05 17:32:13 +0500

    LAB 5

---
+++ program16.asm
@@ -0,0 +1,30 @@
+;Program to access array using indirect opperator
+
+.model small
+.stack 100h
+.data
+
+arr1 db '1','2','3','4' 
+.code                  
+
+Main Proc
+     mov ax,@data   
+     mov ds,ax
+     
+     mov si,offset arr1 
+     mov cx,4
+     l1:
+     mov dx,[si]
+     mov ah,2
+     int 21h
+     inc si
+     loop l1
+     mov ah,4ch
+     int 21h
+Main EndP
+End Main
+
+
+
+
+
