310f67e47bbbdfbda186d31ca4eb834868f8fe0c
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-11-08 17:53:09 +0500

    LAB 5

---
+++ program20(nested loop pattern 2).asm
@@ -0,0 +1,42 @@
+;Program to print the following pattern right angle triangle using loops
+;*
+;**
+;***
+;****
+;*****
+.model small
+.stack 100h
+.data
+start1 dw 1
+.code                  
+
+Main Proc
+mov ax,@data
+mov ds,ax
+mov cx,5
+loop1:
+mov bx,cx
+mov cx,start1
+loop2:
+mov dl,"*"
+mov ah,2
+int 21h   
+loop loop2
+inc start1
+mov cx,bx
+mov dl,10
+mov ah,2
+int 21h
+mov dl,13
+mov ah,2
+int 21h   
+loop loop1
+mov ah,4ch
+int 21h
+
+
+
+Main EndP
+End Main
+
+
