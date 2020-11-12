8f56fbff53c5108d343c4fbdafe588316032aa0a
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-11-12 18:17:44 +0500

    LAB 7

---
+++ program29.asm
@@ -0,0 +1,38 @@
+
+; Addition and Subraction using nested proc
+
+.model small
+.stack 100h
+.data
+
+
+.code                  
+
+Main Proc
+     call addition
+     mov ah,4ch
+     int 21h
+Main EndP
+addition Proc
+    call subraction
+    mov al,3
+    mov bl,3
+    add al,bl
+    add al,48
+     mov dl,al
+    mov ah,2
+    int 21h 
+    ret
+    addition EndP
+subraction Proc
+    mov al,4
+    mov bl,2
+    sub al,bl
+    add al,48
+    mov dl,al
+    mov ah,2
+    int 21h
+    ret
+    subraction EndP
+End Main
+
